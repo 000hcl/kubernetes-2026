d9a92f0665f843451c620c2158bf2499da472016
000hcl <mnrjohansson@gmail.com>
2026-06-22 15:48:50 +0300

    ex 1.5

--- todo-app/index.js
+++ todo-app/index.js
@@ -2,6 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  res.send('ok')
+})
+
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server started in port ${PORT}`)
